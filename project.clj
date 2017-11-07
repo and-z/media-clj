33949adf9fe0d658fc32aac01e7eaed6785d9536
Andreas Zimpel <mail4andz@gmail.com>
2017-11-07 23:52:11 +0100

    Explore the metadata extractor library a bit

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.drewnoakes/metadata-extractor "2.10.1"]]
   :main ^:skip-aot media-clj.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/media_clj/core.clj
+++ src/media_clj/core.clj
@@ -1,5 +1,36 @@
 (ns media-clj.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.java.io :as io])
+  (:import [java.io BufferedInputStream FileInputStream]
+           [com.drew.imaging ImageMetadataReader]))
+
+(defn get-meta [filename]
+  (let [logo (io/resource filename)
+        metadata (ImageMetadataReader/readMetadata (io/file logo))]
+    metadata))
+
+(defn get-directories [metadata]
+  (let [m metadata]
+    (for [d (.getDirectories m)]
+      d)))
+
+(defn get-tags [directory]
+  (for [t (.getTags directory)]
+    {;:tag.type (.getTagType t),
+     :tag.description (.getDescription t),
+     :tag.name (.getTagName t)}))
+
+(defn get-errors [directory]
+  (for [e (.getErrors directory)]
+    e))
+
+(->> (get-meta "clojure-logo.png")
+    (get-directories)
+    (map (fn [d]
+           {:directory.name (.getName d),
+            :tags (get-tags d)
+            :errors (get-errors d)}))
+    (clojure.pprint/pprint))
 
 (defn -main
   "I don't do a whole lot ... yet."
