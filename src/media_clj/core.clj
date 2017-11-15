86a1173f9d31a32eee528f57162a829b87eafd46
Andreas Zimpel <mail4andz@gmail.com>
2017-11-16 00:22:24 +0100

    Implement simple processing using new approach.

--- src/media_clj/core.clj
+++ src/media_clj/core.clj
@@ -1,61 +1,131 @@
 (ns media-clj.core
   (:gen-class)
   (:require [clojure.core.async :as a]
+            [clojure.string :as str]
             [clojure.pprint]
             [clojure.java.io :as io])
-  (:import [java.io BufferedInputStream FileInputStream]
+  (:import [java.util Date]
+           [java.io BufferedInputStream FileInputStream]
            [java.text SimpleDateFormat]))
-
 
 (defn path->file [path]
   (io/file path))
 
-(def target-parent-path "/home/andz/dev/projects/media-clj/out")
+(defn format-with [timestamp formatter]
+  (.format formatter timestamp))
 
-(def sdm (SimpleDateFormat. "YYYY/MM"))
+(defn ext [filename]
+  (let [extension (str/lower-case (subs filename (str/last-index-of filename \.)))]
+    (if (contains? #{".jpg", ".jpeg", ".mts", ".mp4"} extension)
+      extension)))
+
+(defn drop-ext [filename]
+  (subs filename 0 (str/last-index-of filename \.)))
+
+(defn file->filemap [file {:keys [target-parent-path lm-format-fn]}]
+  (let [name (.getName file)
+        extension (ext name)
+        lm (.lastModified file)
+        lm-formatted (lm-format-fn lm)
+        target-path-fn (fn [name-short]
+                  (str (str/join "/" [target-parent-path lm-formatted name-short]) extension))]
+    {:file file,
+     :file/name name,
+     :file/name-short (drop-ext name),
+     :file/origin-path (.getAbsolutePath file),
+     :file/last-modified-ts lm,
+     :file/last-modified-date lm-formatted,
+     :file/target-path (str/join "/" [target-parent-path lm-formatted name]),
+     :file/target-path-fn target-path-fn,
+     :file/target-parent-path (str target-parent-path "/" lm-formatted),
+     :file/extension (or extension :ext-not-supported)}))
+
+(defn get-files [path options]
+  (->> (path->file path)
+       (file-seq)
+       (filter (fn [file] (.isFile file)))
+       (map #(file->filemap % options))))
+
+(defn remove-dir
+  "Does not remove recursively."
+  [path]
+  (let [dir (io/file path)
+        files (->> (file-seq dir)
+                   (filter (fn [file] (.isFile file))))]
+    (doseq [file files]
+      (io/delete-file file))
+    (io/delete-file dir)))
+
+(comment
+  (remove-dir "/home/andz/dev/projects/media-clj/out")
+)
+
+(defn watch-errors [ch a]
+  (a/go-loop []
+    (a/<! ch)
+    (swap! a update-in [:errors] inc)
+    (recur)))
+
+
+(defn count-events [ch a]
+  (a/go-loop []
+    (a/<! ch)
+    (swap! a update-in [:all] inc)
+    (recur)))
+
+
+(defn copy-file! [source target a]
+  (io/make-parents target)
+  (io/copy source target)
+  (swap! a update-in [:copied] inc))
+
+(defn safe-copy-file! [file a]
+  (let [{:file/keys [target-path target-path-fn name-short]} file
+        target-file (io/file target-path)]
+    (if (.exists target-file)
+      (let [new-filename (target-path-fn (format "%s-%d" name-short (System/currentTimeMillis)))]
+        (copy-file! (:file file) (io/file new-filename) a)
+        (swap! a update-in [:duplicates] inc))
+      (copy-file! (:file file) target-file a))))
+
+(defn watch-copy-files [ch a]
+  (a/go-loop []
+    (let [{:keys [file]} (a/<! ch)]
+      (safe-copy-file! file a)
+      (recur))))
+
+(defn stream-files [ch {:keys [path], :as options}]
+  (doseq [{:file/keys [extension name], :as file} (get-files path options)]
+    (if (= :ext-not-supported extension)
+      (a/go (a/>! ch {:topic :errors :error (str name " has unsupported extension")}))
+      (a/go
+        (a/>! ch {:topic :stats})
+        (a/>! ch {:topic :copy :file file})))))
 
 (defn format-with [timestamp formatter]
   (.format formatter timestamp))
 
-(defn file->filemap [file]
-  (let [name (.getName file)
-        lm (.lastModified file)
-        lm-formatted (-> lm (format-with sdm))]
-    {:file file,
-     :file/name name
-     :file/last-modified-ts lm,
-     :file/last-modified-date lm-formatted,
-     :target-parent-path (str target-parent-path "/" lm-formatted),
-     :extension-normalized (identity name)}))
+;;; App configuration
+(def =events= (a/chan))
+(def events-publication (a/pub =events= (fn [t] (:topic t))))
 
-(defn get-files [path]
-  (->> (path->file path)
-       (file-seq)
-       (filter (fn [file] (.isFile file)))
-       (map file->filemap)))
+(def =statistics= (a/chan))
+(def =errors= (a/chan))
+(def =copy= (a/chan))
 
-(comment
-  (->> (get-files "/media/sf_share/test-media/source")
-      (map :file/name)
-      (clojure.pprint/pprint))
+(a/sub events-publication :stats =statistics=)
+(a/sub events-publication :errors =errors=)
+(a/sub events-publication :copy =copy=)
 
- (def files-ch (a/chan))
+(def stats (atom {:all 0,
+                  :copied 0,
+                  :duplicates 0,
+                  :errors 0}))
 
- (a/go
-   (while true
-     (let [file (a/<! files-ch)]
-       (println "received" (:file/name file)))))
+(watch-errors =errors= stats)
+(count-events =statistics= stats)
+(watch-copy-files =copy= stats)
 
- (doseq [file (get-files "/media/sf_share/test-media/source")]
-   (a/>!! files-ch file)))
-
-;; media files processing pipeline
-;; 1. list of all files from given root directory
-;; 2. create filtered sequence of files to be moved // relevant for stats
-;; 2.1. represent via
-;;      {:file #file-object
-;;       :file/last-edited #timestamp,
-;;       :source-path "path/to/file",
-;;       :target-parent-dir "path/to/parent/dir/" // will be concatenated with YYYY/MM/ pattern
-;;       :target-path nil, // to be calculated based on :file/last-edited
-;;       :normalized-extension #{jpg, mts, mp4}}
+(stream-files =events= {:path "/media/sf_share/test-media/source",
+                        :target-parent-path "/home/andz/dev/projects/media-clj/out",
+                        :lm-format-fn #(format-with % (SimpleDateFormat. "YYYY/MM"))})
